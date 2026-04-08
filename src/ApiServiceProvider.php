d50f321b8f5e81e4bd4e472a4ec5f34bc6bae52d
Russell Jones <jonesrussell42@gmail.com>
2026-04-08 12:57:28 -0400

    feat(#1129): SSR decouple + provider HTTP routers; close #1134
    Closes #1129
    Closes #1134
    
    Includes fix(ssr): nullsafe getLanguageResolver in stripLanguagePrefixForRouting.

--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace Waaseyaa\Api;
 
+use Waaseyaa\Api\Http\Router\DiscoveryRouter;
 use Waaseyaa\Entity\EntityTypeManager;
+use Waaseyaa\Foundation\Kernel\HttpKernel;
 use Waaseyaa\Foundation\ServiceProvider\ServiceProvider;
 use Waaseyaa\Routing\WaaseyaaRouter;
 
 final class ApiServiceProvider extends ServiceProvider
 {
     public function register(): void {}
+
+    public function httpDomainRouters(?HttpKernel $httpKernel = null): iterable
+    {
+        if ($httpKernel === null) {
+            return [];
+        }
+
+        return [
+            new DiscoveryRouter(
+                $httpKernel->getDiscoveryApiHandler(),
+                $httpKernel->getEntityTypeManager(),
+            ),
+        ];
+    }
 
     public function routes(WaaseyaaRouter $router, ?EntityTypeManager $entityTypeManager = null): void
     {
